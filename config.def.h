8678527cc80dc2075381198926cb0ff0fcbbe01c
Mateus Auler <mateusauler@protonmail.com>
2020-07-22 14:08:15 -0300

    Changed the default configuration

--- config.def.h
+++ config.def.h
@@ -3,8 +3,8 @@
 
 static int instant = 0;
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 300;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
@@ -17,9 +17,9 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines      = 0;
-static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
-static unsigned int columns    = 0;
+static unsigned int lines      = 20;
+static unsigned int lineheight = 32;         /* -h option; minimum height of a menu line     */
+static unsigned int columns    = 4;
 
 /*
  * Characters not considered part of a word while deleting words
